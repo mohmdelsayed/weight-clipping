560c7cb9244de3be73663a221d21aafa7f64e37b
Mohamed Elsayed <mohamed.elsayed.1995@gmail.com>
2024-03-06 14:32:05 -0500

    change the normalization step in cifar10

--- core/task/cifar10_offline.py
+++ core/task/cifar10_offline.py
@@ -70,7 +70,8 @@ class CIFAR10:
             transform=torchvision.transforms.Compose(
                 [
                     torchvision.transforms.ToTensor(),
-                    torchvision.transforms.Normalize((0.5,), (0.5,)),
+                    # torchvision.transforms.Normalize((0.5,), (0.5,)),
+                    torchvision.transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
                 ]
             ),
         )
